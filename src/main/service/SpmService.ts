673fe97f8c4e8b34625b39b1dfc3e6e1bd2aa140
xxc <778994556@qq.com>
2020-07-28 15:50:08 +0800

    update

--- src/main/service/SpmService.ts
+++ src/main/service/SpmService.ts
@@ -1,7 +1,5 @@
 import SpmDao from "../dao/SpmDao";
 import BaseService from "./abstract/BaseService";
-// @ts-ignore
-import * as gm from "gmtaobao";
 
 export default class SpmService extends BaseService<SpmDao, {}> {
     constructor(context) {
