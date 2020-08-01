50698e5a7a62ffcf920c956d7ec782a173ae8be0
xxc <778994556@qq.com>
2020-08-01 08:15:13 +0800

    更新解析parseExcel对于定义了表头只要表头中的单元格为空就不能通过的bug

--- src/main/utils/Utils.ts
+++ src/main/utils/Utils.ts
@@ -79,27 +79,39 @@ export default class Utils {
     /**
      * 将excelBuffer转换为json
      * @param buffer
-     * @param defineHeader 定义表头与键
+     * @param defineHeader
      * @param who  读取第几张表
      */
     static parseExcel(buffer, defineHeader: any = {}, who: number = 0) {
         let data, workbook, header;
-        workbook = xlsx.read(buffer, {type: "buffer"});
+        workbook = xlsx.read(buffer, {
+            type: "buffer"
+        });
         //读取表的数据
         data = workbook.Sheets[workbook.SheetNames[who]];
         data = xlsx.utils.sheet_to_json(data);
         try {
+            let header = true;
             //映射对应的键
             data = data.map(v => {
                 let o = {};
                 for (let key in defineHeader) {
                     let targetKey = defineHeader[key];
-                    //如果表中没有对应的键
-                    if (typeof v[targetKey] === "undefined") {
-                        throw "缺少字段" + targetKey
+                    //如果是表头
+                    if (header === true) {
+                        //如果表头中没有对应的键
+                        if (typeof v[targetKey] === "undefined") {
+                            throw "缺少字段" + targetKey
+                        }
+                        let regex = /(time|时间)/i;
+                        //如果包含时间
+                        if (regex.test(key) || regex.test(targetKey)) {
+                            v[targetKey] = this.parseExcelDate(v[targetKey]) || v[targetKey];
+                        }
                     }
                     o[key] = v[targetKey];
                 }
+                header = false;
                 return o;
             });
         } catch (e) {
