cc5e8607d209c9f60ed18f124f0e8604a76ff8cd
xxc <778994556@qq.com>
2020-11-16 15:46:22 +0800

    update

--- src/service/PrizeService.ts
+++ src/service/PrizeService.ts
@@ -66,19 +66,19 @@ export default class PrizeService extends BaseService<PrizeDao<Prize>, Prize> {
                 if (prize.type === "goods") {
                     let {skuId, itemId} = prize[prize.type];
                     r.data = await topService.opentradeSpecialUsersMark(skuId, itemId);
-                    this.spm("mark");
+                    await this.spm("mark");
                 }
                 //积分领取
                 else if (prize.type === "point") {
                     let {addPointNum} = prize[prize.type];
                     r.data = await topService.taobaoCrmPointChange(addPointNum);
-                    this.spm("point");
+                    await this.spm("point");
                 }
                 //权益领取
                 else if (prize.type === "benefit") {
                     let {ename} = prize[prize.type];
                     r.data = await topService.sendBenefit(ename);
-                    this.spm("benefit");
+                    await this.spm("benefit");
                 }
             }
         }
