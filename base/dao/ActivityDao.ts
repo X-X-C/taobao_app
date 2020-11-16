1b76f885bab4a12c7ca236ff32d52dab6608ea5f
xxc <778994556@qq.com>
2020-11-16 16:23:15 +0800

    delete base

--- base/dao/ActivityDao.ts
+++
@@ -1,7 +0,0 @@
-import BaseDao from "../../base/dao/abstract/BaseDao";
-
-export default class ActivityDao<T extends object> extends BaseDao<T> {
-    constructor(context) {
-        super(context, "activities");
-    }
-}
--- base/utils/Type.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 内部返回对象
- */
-export type result = {
-    code?: number,
-    message?: string,
-    data?: any,
-    [props: string]: any
-}
