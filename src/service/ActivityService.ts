e8056b8ef851c3c40e28e05d7ad2d7ddee36e262
xxc <778994556@qq.com>
2020-11-24 17:26:16 +0800

    update

--- src/service/ActivityService.ts
+++ src/service/ActivityService.ts
@@ -44,7 +44,7 @@ export default class ActivityService extends BaseActivityService {
                 let userService = this.getService(UserService);
                 let rankPrizeList = activity.data.config.rankPrizeList;
                 //需要开奖的数据
-                let rankList = (await userService.rank()).list;
+                let rankList = (await userService.rank()).data.list;
                 //需要开奖的奖品
                 let winners = [];
                 for (let user of rankList) {
