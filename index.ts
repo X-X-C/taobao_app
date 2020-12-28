8b93df308b36a1738489bb4b802befa21095b7d9
xxc <778994556@qq.com>
2020-12-28 15:27:35 +0800

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import App from "./base/App";
 // @ts-ignore
 exports.main = async (context) => {
     const app = new App(context, "main");
-    app.config.globalActivity = true;
     return await app.run(async function () {
         //..
     });
