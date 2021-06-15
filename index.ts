89ac510fadbb62066f49d2c0280eb963c2e68edd
xxc <778994556@qq.com>
2021-06-15 17:56:55 +0800

    update

--- index.ts
+++ index.ts
@@ -1,22 +1,9 @@
-import App from "./App";
 import UserService from "./src/service/UserService";
 import PrizeService from "./src/service/PrizeService";
 import SpmService from "./src/service/SpmService";
 import {XApp} from "./base/App";
+import App from "./App";
 
 // tsconfig.json配置 importsNotUsedAsValues 的值可以控制没被使用的导入语句将会被如何处理
-const modules = [UserService, PrizeService, SpmService];
-for (let entry of Object.entries(XApp.exports)) {
-    // @ts-ignore
-    exports[entry[0]] = async (context) => {
-        const app = new App(context, entry[0]);
-        app.runNeedParams = entry[1].params || {};
-        if (!entry[1].needGlobalParam) {
-            app.globalNeedParams = {};
-        }
-        entry[1].before.forEach(v => v.call(app.before))
-        return await app.run(async function () {
-            await app.getService(entry[1].constructor)[entry[0]]();
-        });
-    }
-}
+[UserService, PrizeService, SpmService];
+exports = XApp.initExpose(App);
