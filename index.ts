1bf0038778d79ba98fb4c88fbcab3683282a4fd1
xxc <778994556@qq.com>
2021-06-15 17:58:20 +0800

    update

--- index.ts
+++ index.ts
@@ -2,18 +2,5 @@ import App from "./App";
 import {XApp} from "./base/App";
 import UserService from "./src/service/UserService";
 
-const services = [UserService];
-for (let entry of Object.entries(XApp.exports)) {
-    // @ts-ignore
-    exports[entry[0]] = async (context) => {
-        const app = new App(context, entry[0]);
-        app.runNeedParams = entry[1].params || {};
-        if (!entry[1].needGlobalParam) {
-            app.globalNeedParams = {};
-        }
-        entry[1].before.forEach(v => v.call(app.before))
-        return await app.run(async function () {
-            await app.getService(entry[1].constructor)[entry[0]]();
-        });
-    }
-}
+[UserService];
+exports = XApp.initExpose(App);
