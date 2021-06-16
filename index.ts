1859f6f7bb45d1143b820913c3981850cd833b41
xxc <778994556@qq.com>
2021-06-16 11:26:28 +0800

    update

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import App from "./App";
 import {XApp} from "./base/App";
-import UserService from "./src/service/UserService";
 
-const modules = [UserService];
 // @ts-ignore
 XApp.initExpose(App, exports);
